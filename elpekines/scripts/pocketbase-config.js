971e519e0759cd6f6bb177d5b8033c15b7b2272b
jcarpi1510-boop <jcarpi1510@gmail.com>
2026-03-15 20:11:41 -0300

    Update pocketbase-config.js

--- elpekines/scripts/pocketbase-config.js
+++ elpekines/scripts/pocketbase-config.js
@@ -1,5 +1,5 @@
 // --- PocketBase Centralized Configuration ---
-const pb = new PocketBase('http://127.0.0.1:8090'); // URL por defecto para PocketBase local
+const pb = new PocketBase('https://jesus151083-el-pekines-db.hf.space'); // URL de tu base de datos en Hugging Face
 
 // Hacerlo global para que los otros scripts lo usen
 window.pb = pb;
